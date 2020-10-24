2d2b7e5a9f6803ac971eb64dcfaeb61f6cb6d251
zkan <zkan.cs@gmail.com>
2020-10-24 16:16:57 +0700

    Add SQS code example :bear:

---
+++ receive.py
@@ -0,0 +1,36 @@
+import os
+
+import boto3
+
+# Create SQS client
+sqs = boto3.client(
+    'sqs',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+)
+
+queue_url = 'https://sqs.ap-southeast-1.amazonaws.com/815882715903/dev-location-data'
+
+# Receive message from SQS queue
+response = sqs.receive_message(
+    QueueUrl=queue_url,
+    AttributeNames=[
+        'SentTimestamp'
+    ],
+    MaxNumberOfMessages=1,
+    MessageAttributeNames=[
+        'All'
+    ],
+    VisibilityTimeout=0,
+    WaitTimeSeconds=0
+)
+
+message = response['Messages'][0]
+receipt_handle = message['ReceiptHandle']
+
+# Delete received message from queue
+sqs.delete_message(
+    QueueUrl=queue_url,
+    ReceiptHandle=receipt_handle
+)
+print('Received and deleted message: %s' % message)
---
+++ send.py
@@ -0,0 +1,38 @@
+import os
+
+import boto3
+
+# Create SQS client
+sqs = boto3.client(
+    'sqs',
+    aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+)
+
+queue_url = 'https://sqs.ap-southeast-1.amazonaws.com/815882715903/dev-location-data'
+
+# Send message to SQS queue
+response = sqs.send_message(
+    QueueUrl=queue_url,
+    DelaySeconds=10,
+    MessageAttributes={
+        'Title': {
+            'DataType': 'String',
+            'StringValue': 'The Whistler'
+        },
+        'Author': {
+            'DataType': 'String',
+            'StringValue': 'John Grisham'
+        },
+        'WeeksOn': {
+            'DataType': 'Number',
+            'StringValue': '6'
+        }
+    },
+    MessageBody=(
+        'Information about current NY Times fiction bestseller for '
+        'week of 12/11/2016.'
+    )
+)
+
+print(response['MessageId'])
